d316c07fb30c198fa9e048fb2bc240e94ca32366
Eric Edgeworth <ericedge@gmail.com>
2012-08-11 16:26:11 -0700

    DungeonMaster improvements
    Making the DungeonMaster the primary object for the
    overall running of the game, trying to figure out
    the way to represent the game world itself.

--- ruby/adventure.rb
+++ ruby/adventure.rb
@@ -53,6 +53,7 @@ class Entity
     end
 
     def show_contents()
+        # Yeah, inefficient
         ObjectSpace.each_object(Entity) do |e|
             puts e if e.container == self
         end
@@ -71,33 +72,94 @@ class Room < Entity
     # TODO: consider using ObjectSpace._id2ref/object_id to connect Rooms?
     # Or maybe makes more sense to have a hash of Rooms and each
     # Room knows its exits refer to keys for other Rooms' values?
+    # Or a hash of direction -> room object
+    attr_accessor :exits
 end
 
-class DungeonMaster < Entity
+class DungeonMaster
+
+    attr_accessor :current_location, :rooms
+
+    def initialize()
+        @rooms = { "home_base" => Room.new("HOMEBASE", 
+                    "Sort-of pentagon shape in ground"),
+                "first_base" => Room.new("FIRSTBASE",
+                    "Soft square on ground."),
+                "second_base" => Room.new("SECONDBASE",
+                    "Soft square on ground. Deja vu."),
+                "third_base" => Room.new("THIRDBASE",
+                    "Soft square on ground. So much for original writing.") 
+                 }
+        @current_location = @rooms["home_base"]
+    end
+
     # display current room description, a prompt character,
     # and accept+process user input
-    # TODO: What's the right object class here?  Prompt? 
-    # Interface? Narrator? DungeonMaster?
-    # TODO: Do we loop infinitely in here, or in the main program?
+    def run_game()
+        while 1 do
+            puts @current_location
+            print "> "
+            begin
+                command = gets.chomp
+                do_thy_bidding(command)
+            rescue
+                # ctrl-D and chomp do not mix
+                puts "\nDOES NOT COMPUTE\nType EXIT to exit!"
+                command = ''
+            end
+        end
+    end
+
+    # Just test the various entities, make sure
+    # stuff is working properly, no strange behaviors
+    def test_entities
+        start_room = Room.new("HOME PLATE", "This is where you start")
+        inventory = Entity.new("INVENTORY", "The things you are carrying")
+        bag = Item.new("BAG", "This is your bag")
+        torch = Item.new("TORCH", 
+            "It burns brightly with the flame of justice!")
+        torch.container = inventory
+        torch.action("LIGHT")
+        #puts Item.inventory.inspect
+        puts "Current inventory:"
+        inventory.show_contents
+        bag.container = inventory
+        puts "Current inventory with bag:"
+        inventory.show_contents
+        puts start_room
+    end
+
+    def do_thy_bidding(command)
+        # TODO: more command processing
+        command.upcase!
+        words = command.split()
+        if words[0] == "EXIT"
+            puts "Thanks for playing!"
+            exit
+        elsif words[0] == "TEST"
+            test_entities()
+        else
+            puts "Sorry, I don't understand \"%s\"" % [command]
+        end
+    end
+
+    def find_entity(name)
+        # given the name of an entity, return its 
+        # object if the entity is within reach,
+        # or null otherwise.
+    end
+
 end
 
 class Player < Entity
     # TODO: make fancy.
+    # So far the only purpose I can think to have a Player
+    # object is for inventory, which can be represented
+    # as player_inventory in DungeonMaster, so maybe no 
+    # need for this class?
 end
 
-def test_entities
-    inventory = Entity.new("INVENTORY", "The things you are carrying")
-    bag = Item.new("BAG", "This is your bag")
-    torch = Item.new("TORCH", "It burns brightly with the flame of justice!")
-    torch.container = inventory
-    torch.action("LIGHT")
-    #puts Item.inventory.inspect
-    puts "Current inventory:"
-    inventory.show_contents
-    bag.container = inventory
-    puts "Current inventory with bag:"
-    inventory.show_contents
+if __FILE__ == $PROGRAM_NAME
+    DungeonMaster.new().run_game()
 end
-
-test_entities()
 
